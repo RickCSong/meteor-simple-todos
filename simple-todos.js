182263704521ee1b68d9a3f67404717c231e5468
Rick Song <ricksong@squareup.com>
2015-02-24 23:52:47 -0800

    Basic CRUD

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,38 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" },
-    ]
+    tasks: function() {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {$set: {checked: !this.checked}});
+    },
+
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
   });
 }
 
